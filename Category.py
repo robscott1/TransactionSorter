0cbdd555c1e3122140e6dc602d6787cf7b7bff19
kscott27 <kmscott27@gmail.com>
2019-03-02 14:05:42 -0800

    Added a "Category" class

---
+++ Category.py
@@ -0,0 +1,18 @@
+class Category():
+
+  def __init__(self, name, keywords):
+    self.name = name
+    self.keywords = keywords
+    self.total = 0
+
+  def addAmount(self, amt):
+    self.total += amt
+
+  def containsWord(self,phrase,word):
+    return word in phrase
+
+  def checkKeywords(self, location):
+    for keyword in self.keywords:
+      if self.containsWord(location,keyword):
+        return True
+    return False
--- TransactionSorter.py
+++ TransactionSorter.py
@@ -1,22 +1,28 @@
 import csv
+from Category import Category
 
 def contains_word(phrase,word):
   return word in phrase
 
-f = open('Feb19.csv')
+safeway = Category("Safeway", ["SAFEWAY"])
+groceries = Category("Groceries", ["SAFEWAY","STARBUCK"])
+bars = Category("Bars", ["MCLINTOCK","CREEKY","BULL'S","JAXSON","FROG & PEACH","BLUELIGHT","BARRELHOUSE"])
+breakfast = Category("Breakfast",["HOMEGROWN", "GOOD EATS", "LINCOLNMARKETDELI"])
+coffee = Category("Coffee", ["STARBUCK","SPECIALTYS","BLACKHORSE","PEET'S"])
+restaurants = Category("Restaurants", ["PATRIOT HOUSE","CHIPOTLE","IN N OUT","SQ *","SOMA CHICKEN","DOMINO'S"])
+movies = Category("Movies", ["Prime Video"])
+pharmacy = Category("Pharmacy", ["WALGREENS"])
+internet = Category("Internet", ["COMCAST"])
+
+f = open('../Feb19.csv')
 lines = f.readlines()
 
-categories = {
-  "Safeway": [],
-  "Starbucks": []
-
-}
+categories = [safeway,groceries,bars,breakfast,coffee,restaurants,movies,pharmacy,internet]
 
 amount_charged = []
 amount_paid = []
 location_ = []
 unhandled = []
-safeway = []
 
 for line in lines:
     num_strs = line.split(',')
@@ -33,12 +39,15 @@ for line in lines:
       print("$" + amount + " was paid at " + location)
 
     location_.append(location)
-    if contains_word(location,"SAFEWAY"):
-      categories.get("Safeway").append(float(amount))
-    elif contains_word(location,"STARBUCK"):
-      categories.get("Starbucks").append(float(amount))
-    else:
-      unhandled.append([amount,location])
+
+    match = False
+    for category in categories:
+      if category.checkKeywords(location):
+        category.addAmount(float(amount))
+        match = True
+
+    if match == False:    
+      unhandled.append(location)
 
 
 total_charged = sum(amount_charged)
@@ -46,10 +55,10 @@ total_paid = sum(amount_paid)
 print("Total charged: " + str(total_charged))
 print("Total paid: " + str(total_paid))
 
-for key in categories:
-  print("Total charged at " + key + ": " + str(sum(categories.get(key))))
-
-for pair in unhandled:
-  print("[UNHANDLED ROW] " + "Location: " + pair[1] + ", Amount: " + pair[0])
-
 print("Total unhandled rows: " + str(len(unhandled)))
+
+for category in categories:
+  print(category.name + ": " + str(category.total))
+
+for location in set(unhandled):
+  print("[UNHANDLED LOCATION] " + location)
