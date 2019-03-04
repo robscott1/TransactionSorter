e9fb8b43d26789b2805701a8db36903f71316da8
kscott <kmscott27@gmail.com>
2019-03-03 20:53:47 -0800

    Started splitting the code into more dynamic modules

---
+++ Sorter.py
@@ -0,0 +1,85 @@
+import csv
+import xlsxwriter
+from Category import Category
+from ExcelManager import ExcelManager
+
+class Sorter():
+
+  def execute(self, parser, categories):
+    categoryFile = open('../Kevin Cash Flow Forecasting - Credit Card Charges.csv')
+    lines = categoryFile.readlines()
+
+    ExpectedCategories = {}
+
+    firstLine = True
+    for line in lines:
+      if firstLine == True:
+        firstLine = False
+      else:
+        listLine = line.split(',')
+        categoryName = listLine[0]
+        if listLine[3] != "":
+          alottedAmt = float(listLine[3])
+        else:
+          alottedAmt = float(0)
+
+        ExpectedCategories[categoryName] = alottedAmt
+
+    importFileName = "../TimFeb19"
+    f = open(importFileName + ".csv")
+    lines = f.readlines()
+    
+    amount_charged, amount_paid, unhandled = parser.execute(lines, categories)
+    
+    
+    total_charged = sum(amount_charged)
+    total_paid = sum(amount_paid)
+    
+    exportFileName = importFileName + "Report.xlsx"
+    with open(exportFileName, 'w', newline='') as exportFile:
+      workbook = xlsxwriter.Workbook(exportFileName)
+      worksheet = workbook.add_worksheet()
+      red_highlight = workbook.add_format()
+      red_highlight.set_bg_color('red')
+      bold_text = workbook.add_format({'bold': True})
+      w = ExcelManager(worksheet)
+    
+      w.writerow(["Category Overview"], bold_text)
+      w.writerow(["Status", "Expenditure Category", "Alotted Amount", "Amount Spent", "Delta"], bold_text)
+    
+      delta = total_charged + ExpectedCategories.get("Total")
+      print("Total charged: " + str(total_charged))
+      if delta >= 0:
+        w.writerow([None, "Total charged:", ExpectedCategories.get("Total"), total_charged, delta])
+      else:
+        w.writerow([None, "Total charged:", ExpectedCategories.get("Total"), total_charged, delta], red_highlight)
+    
+      print("Total paid: " + str(total_paid))
+      w.writerow([None, "Total paid:", None, total_paid])
+    
+      for key in ExpectedCategories:
+        for category in categories:
+          if key == category.name:
+            delta = category.total + ExpectedCategories.get(key)
+            if delta < 0:
+              print( "OVER-SPENDING OCCURRED! Category: " + key + ", Delta: $" + str(delta) )
+              w.writerow(["OVER-SPENDING OCCURRED!", key, ExpectedCategories.get(key), category.total, delta], red_highlight)
+            else:
+              print( "                        Category: " + key + ", Delta: $" + str(delta) )
+              w.writerow([None, key, ExpectedCategories.get(key), category.total, delta])
+    
+      w.writerow()
+      w.writerow(["ATTENTION: Kevin's awesome program did not find keyword matches for the following transactions:"], bold_text)  
+      print("Total unhandled rows: " + str(len(unhandled)))
+      w.writerow(["Total unhandled transactions: " + str(len(unhandled)) + " out of " + str(len(lines)) + " total"], bold_text)
+    
+      unaccountedSum = 0
+      for pair in unhandled:
+        print("[UNHANDLED LOCATION] " + pair[0] + ", Amount charged: " + str(pair[1]) )
+        unaccountedSum += pair[1]
+        w.writerow(["[UNHANDLED TRANSACTION]", "Location:", pair[0], "Amount:", pair[1]])
+    
+      print("Miscellaneous (Uncategorized amounts): " + str(unaccountedSum))
+      w.writerow(["Miscellaneous (Uncategorized amounts):" + str(unaccountedSum)])
+    
+      workbook.close()
---
+++ TimAmexApp.py
@@ -0,0 +1,103 @@
+from Category import Category
+
+class TimAmexApp():
+
+  def __init__(self, sorter):
+    self.sorter = sorter
+    self.parser = Parser()
+
+    self.umbrellaInsurance =   Category("Umbrella Insurance")
+    self.autoTransportation =  Category("Auto Transportation Costs")
+    self.uberTransportation =  Category("Uber Transportation Costs")
+    self.autoRepairs =         Category("Auto Repairs")
+    self.autoRegistration =    Category("Auto Registration")
+    self.autoInsurance =       Category("Auto Insurance")
+    self.autoLoanPayment =     Category("Auto Loan Payment")
+    self.hbo =                 Category("Amazon Prime HBO")
+    self.tech =                Category("Technology Replace/Upgrade Costs")
+    self.essentials =          Category("Toiletries/Essentials", ["ROGUE"])
+    self.contacts =            Category("Contacts")
+    self.medicalOOP =          Category("Medical OoP Costs")
+    self.mdvip =               Category("MDVIP Membership")
+    self.payments =            Category("Online Payments", ["ONLINE PAYMENT","PAYPAL"])
+    self.safeway =             Category("Safeway", ["SAFEWAY"])
+    self.groceries =           Category("Groceries/Food/Sundry Items", ["SAFEWAY","STARBUCK","SPECIALTYS","BLACKHORSE","PEET'S"])
+    self.bars =                Category("Bars", ["MCLINTOCK","CREEKY","BULL'S","JAXSON",
+      "FROG & PEACH","BLUELIGHT","BARRELHOUSE","MILK BAR","CORK N BOTTLE","CAMPUS BOTTLE"])
+    self.breakfast =           Category("Breakfast",["HOMEGROWN", "GOOD EATS", "LINCOLNMARKETDELI"])
+    self.coffee =              Category("Coffee", ["STARBUCK","SPECIALTYS","BLACKHORSE","PEET'S"])
+    self.restaurants =         Category("Restaurants", ["PATRIOT HOUSE","CHIPOTLE","IN N OUT",
+      "SQ *","SOMA CHICKEN","DOMINO'S","TAQUERIA SANTA CRUZ","PRESSED","HOMEGROWN", "GOOD EATS", "LINCOLNMARKETDELI",
+      "MEAT CO"])
+    self.movies =              Category("Movies", ["Prime Video"])
+    self.pharmacy =            Category("Prescription Drug Costs", ["WALGREENS"])
+    self.internet =            Category("Cable/Internet", ["COMCAST"])
+    self.entertainment =       Category("Entertainment", ["BROWNPAPERTICKETS","FISHER CATCH","MCLINTOCK","CREEKY","BULL'S","JAXSON",
+      "FROG & PEACH","BLUELIGHT","BARRELHOUSE","MILK BAR","CORK N BOTTLE","CAMPUS BOTTLE"])
+    self.shopping =            Category("Clothes/Shoes (me)", ["ADIDAS","CALVIN KLEIN"])
+    self.amazon =              Category("Amazon Prime", ["Amzn","Amazon", "Prime Video"])
+    self.travel =              Category("Personal Travel/Vacation Costs", ["HOSTEL"])
+    self.total =               Category("Total")
+    
+    
+    self.categories = [self.umbrellaInsurance,
+                       self.groceries,
+                       self.autoTransportation,
+                       self.uberTransportation,
+                       self.autoRepairs,
+                       self.autoRegistration,
+                       self.autoInsurance,
+                       self.autoLoanPayment,
+                       self.internet,
+                       self.amazon,
+                       self.hbo,
+                       self.tech,
+                       self.essentials,
+                       self.shopping,
+                       self.contacts,
+                       self.pharmacy,
+                       self.medicalOOP,
+                       self.mdvip,
+                       self.travel,
+                       self.restaurants,
+                       self.entertainment,
+                       self.payments]
+
+  def run(self):
+    self.sorter.execute(self.parser, self.categories)
+
+
+class Parser():
+
+  def execute(self, lines, categories):
+    amount_charged = []
+    amount_paid = []
+    location_ = []
+    unhandled = []
+    
+    for line in lines:
+        num_strs = line.split(',')
+        amount = num_strs[-1]
+        amount = amount[:-1]
+        location = num_strs[0]
+    
+        if float(amount) < 0:
+          amount_charged.append(float(amount))
+          print("$" + amount + " was charged at " + location)
+        elif float(amount) > 0:
+          amount_paid.append(float(amount))
+          print("$" + amount + " was paid at " + location)
+    
+        location_.append(location)
+    
+        match = False
+        for category in categories:
+          if category.checkKeywords(location):
+            category.addAmount(float(amount))
+            match = True
+    
+        if match == False:    
+          unhandled.append([location,float(amount)])
+
+    return amount_charged, amount_paid, unhandled
+
