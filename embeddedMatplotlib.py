448c2db4d05d04d3662008ba67a38a6334d14436
robscott1 <47842575+robscott1@users.noreply.github.com>
2019-07-24 13:51:39 -0700

    Created button widget with sublayout to host all of the plotting buttons. Implemented bar graph plotting functionality

--- embeddedMatplotlib.py
+++ embeddedMatplotlib.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QDialog, QApplication, QPushButton, QVBoxLayout
+from PyQt5.QtWidgets import QDialog, QApplication, QPushButton, QVBoxLayout, QGridLayout
 
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
@@ -8,6 +8,8 @@ import matplotlib.pyplot as plt
 import random
 
 from Application import Application
+
+import numpy as np
 
 class Window(QDialog):
     def __init__(self, App, parent=None):
@@ -27,14 +29,22 @@ class Window(QDialog):
         self.toolbar = NavigationToolbar(self.canvas, self)
 
         # Just some button connected to `plot` method
-        self.button = QPushButton('Plot')
-        self.button.clicked.connect(self.plotPie)
+        self.plotPieBtn = QPushButton('Pie Chart')
+        self.plotPieBtn.clicked.connect(self.plotPie)
+
+        self.plotBarBtn = QPushButton('Bar Graph')
+        self.plotBarBtn.clicked.connect(self.plotBar)
 
         # set the layout
         layout = QVBoxLayout()
         layout.addWidget(self.toolbar)
         layout.addWidget(self.canvas)
-        layout.addWidget(self.button)
+        btnLayout = QGridLayout()
+        layout.addLayout(btnLayout)
+
+        btnLayout.addWidget(self.plotPieBtn, 0, 0)
+        btnLayout.addWidget(self.plotBarBtn, 0, 1)
+
         self.setLayout(layout)
 
     def plotPie(self):
@@ -49,6 +59,44 @@ class Window(QDialog):
 
         self.ax.pie(data, labels=cats)
         self.ax.set_title("Spending by Category")
+
+        self.canvas.draw()
+
+    def plotBar(self):
+        # data to plot
+        cats = self.app.getCategoryNamesList()[1:]
+        n_groups = len(cats)
+        amountSpent = [self.app.getAmountSpentByCategory(c) for c in cats]
+        amountAllotted = [self.app.getAmountAllottedByCategory(c) for c in cats]
+
+        self.figure.clear()
+        # create plot
+        self.ax = self.figure.add_subplot(111)
+
+        self.ax.clear()
+
+        index = np.arange(n_groups)
+        bar_width = 0.35
+        opacity = 0.8
+
+        rects1 = plt.bar(index, amountAllotted, bar_width,
+        alpha=opacity,
+        color='b',
+        label='Allotted')
+
+        rects2 = plt.bar(index + bar_width, amountSpent, bar_width,
+        alpha=opacity,
+        color='g',
+        label='Spent')
+
+        plt.xlabel('Spending')
+        plt.ylabel('Category')
+        plt.title('Allotment and Spending')
+        plt.xticks(index + bar_width, [x for x in self.app.getCategoryNamesList()[1:]])
+        plt.legend()
+
+        plt.tight_layout()
+        #plt.show()
 
         self.canvas.draw()
 
