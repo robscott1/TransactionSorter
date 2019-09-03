6501f3765707cce809157eb3cd75f7b77e300700
robscott1 <47842575+robscott1@users.noreply.github.com>
2019-09-02 20:28:09 -0700

    Commenting functions

--- DataPrepSupport.py
+++ DataPrepSupport.py
@@ -20,6 +20,16 @@ class DataProcessor():
     self.plannedDf = pd.DataFrame(columns=['Date', 'Amount', 'Recurrence','Priority', 'Method', 'Name'])
 
   def getDateOffset(self, startDay):
+    '''
+    Finds the number day of the month to create the dateOffset when date_ranges
+    are made. date_ranges default to the start of the month, so the dateOffset gives
+    the correct day that recurring transactions occur on
+
+    @startDay: timestamp used to be the first date in a date_range for getProjectionData
+
+    @returns: number of days that the date_range will offset from 
+    month start for monthly transactions
+    '''
     startDay = str(startDay)
     startDay = startDay.split()[0]
     startDay = startDay.split('-')
@@ -27,14 +37,27 @@ class DataProcessor():
     return int(day)
 
   def createDateList(self, startDay, rateOfRecurrence):
-    print("")
-    print("each transaction startDay and rateOfRecurrence: ", startDay, rateOfRecurrence)
+    '''
+    Creates list of dates that expenditures will occur for each individual transaction
+    (recurring or singular event)
+
+    @startDay: for recurring transactions, this is the first charge. For singlular transactions,
+    it is just that date the charge will be made on
+
+    @rateOfRecurrence: the rate of recurrence specified in the GUI when planned
+    transaction is made. That rate of recurrence links to a dictionary key-value
+    that points to the Pandas syntax for frequency of the date_range.
+
+    @returns: list of dates that transactions occur on for cash projection analysis
+
+    '''
     if rateOfRecurrence == None:
       return list(startDay)
     
     if rateOfRecurrence == 'monthly':
       return list((pd.date_range(startDay, periods=self.freqKey[rateOfRecurrence][1],
                    freq=self.freqKey[rateOfRecurrence][0]) + pd.DateOffset(days=self.getDateOffset(startDay) - 1)))
+    
     if rateOfRecurrence == 'bi-weekly':
       return list(pd.date_range(startDay, periods=self.freqKey[rateOfRecurrence][1],
                    freq=self.freqKey[rateOfRecurrence][0]))
@@ -42,13 +65,11 @@ class DataProcessor():
   def packageTransactionData(self, date, name, amount, priority, rateOfRecurrence, paymentMethod=None):
     package = TransactionPackage()
     package.name = name
-    print("DPS packageTransactionData date: ", name, date)
     package.date = self.createDateList(date, rateOfRecurrence)
     package.amount = amount
     package.rateOfRecurrence = rateOfRecurrence
     package.priority = priority
     package.paymentMethod = paymentMethod
-    print("package name and date:", package.name, package.date)
     return package
 
   def extractPlannedTransactionData(self, plannedT):
@@ -71,6 +92,20 @@ class DataProcessor():
 
 
   def getUserDataDefinedTransactions(self, allottedAmt, userData):
+    '''
+    Creates the recurring transactions defined in the user-defined "setup"
+    page of the GUI. Instantiates the recurring credit card payment and 
+    income based on specified frequency. Those transactions are then added
+    to the projection dataframe with recurring dates.
+    
+    @allottedAmt: total of all allotted amounts for user-created categories.
+    Estimates how much will be paid for credit card bill each month for 
+    projection purposes
+
+    @userData: user-defined financial standing. Used for cash projection
+    See UserData object for details.
+
+    '''
     creditCardPayment = self.packageTransactionData(userData.nextCreditCardPaymentDate,
                                                      'Credit Card', -1 * allottedAmt, 1, 'monthly')
     
@@ -83,6 +118,23 @@ class DataProcessor():
     self.plannedDf = self.addTransactionToDataframe(incomeTransaction).sort_values(by='Date')
 
   def getProjectionData(self, allottedAmt, userData, plannedTransactions):
+    '''
+    Prepares lists of dates and charges associated with any plannedTransactions,
+    monthly income, and credit card payments for future projection
+
+    @allottedAmt: total cash allotted monthly for all user-defined categories.
+    Used to predict how much user will by paying for their montly credit
+    card bill
+
+    @userData: User-defined data oject that describes the user's
+    current financial standing
+
+    @plannedTransactions: list of all planned transactions that will be included
+    in the plannedDf
+
+    @returns: tuple with a list any dates that have transactions occur, and a list 
+    of the working balance of the user's checking acount over time
+    '''
 
     self.extractPlannedTransactionData(plannedTransactions)
 
--- PandasAgent.py
+++ PandasAgent.py
@@ -10,29 +10,73 @@ class PandasAgent():
     self.dataProcessor = DataProcessor()
 
   def getUserData(self, data):
+    '''
+    Saves UserData object with user-defined financial standing while 
+    swapping the slashes for apostrophes in order to keep all saved dates
+    in the same format
+
+    @data: UserData object that holds user-defined current financial standing
+    Used for future cash projections
+
+    '''
     self.userData = data
     self.userData.nextPayDate = self.userData.nextPayDate.replace('/', '-')
     self.userData.nextCreditCardPaymentDate = self.userData.nextCreditCardPaymentDate.replace('/', '-')
 
   def getCompletedTransactionsDataframe(self, fileName):
+    '''
+    Reads imported CSV file into a Pandas Dataframe. Pandas will be used
+    for all analysis and projection purposes
+
+    See DataPrepSupport.py to see analysis functions 
+    '''
     self.completedDf = pd.read_csv(fileName, sep=',', header = None)
 
   def getProjectionData(self, allottedAmt, plannedT):
+    '''
+    Calls getProjectionData in DataProcessor to send projection data to ProjectionWindow
+
+    @allottedAmt: total cash allotted for all existing categories, used to project
+    credit card charges for each month in ProjectionWindow
+
+    @plannedT: list of PlannedTransaction objects 
+    '''
     return self.dataProcessor.getProjectionData(allottedAmt, self.userData, plannedT)
 
   def getTimeSeriesData(self):
+    '''
+    Prepares the lists of dates on which credit card transactions 
+    took place, and the running total of charges for all
+    transactions in the CSV file.
 
-    # Reverse the order to get df chronologically correct
+    Extracts all dates and amounts of charges from completedDf
+    which is a member of the PandasAgent object
+
+    @returns: 
+    - SpendingByDay(list) a list of the runningBalance of
+    credit card charges over a period of time
+    - ListOfDates(list) a list of dates on which all transactions
+    took place
+    '''
+
+    # Reverse the order to make the DataFrame chronologically correct
     df1 = self.completedDf[::-1]
+
     # Getting the items to plot
     trans = df1.loc[:,'date':'amount']
 
+    # Define variables that will be recorded as the function
+    # iterates through the completedDf
     previousDate = trans.date[len(trans) - 1]
     spendingByDay = []
     listOfDates = []
     dailyTotal = 0
+
     for index, row in trans.iterrows():
         
+        # Because several charges can happen in one day, the function will
+        # total up all charges on the same day before appending it to
+        # spendingByDay accurately tracking spending per day
         if previousDate == row.date:
             dailyTotal += row.amount
             previousDate = row.date
@@ -41,6 +85,8 @@ class PandasAgent():
             spendingByDay.append(abs(round(dailyTotal, 0)))
             ts = pd.to_datetime(row.date)
             dailyTotal += row.amount
+            
+            # Remove the year from the timestamp for visual purposes in the plottingWindow
             date = row.date.split('/')
             date.pop(-1)
             date = '/'.join(date)
@@ -55,7 +101,4 @@ class PandasAgent():
     date = date.split('-')
     day = date[2].split()
     return day[0] == '01'
-
-#if __name__ == "__main__":
-  #print(pd.to_datetime(date.today()))
 
