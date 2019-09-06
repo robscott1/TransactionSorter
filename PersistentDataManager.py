e676b31e23b8021db4b44852e06134bbb76c1abe
robscott1 <47842575+robscott1@users.noreply.github.com>
2019-09-06 12:36:37 -0700

    Added class description

--- PersistentDataManager.py
+++ PersistentDataManager.py
@@ -1,9 +1,17 @@
 import xml.etree.ElementTree as et
 from APIData import TransactionData, CategoryData
-from xmlAgent import xmlAgent
+from XMLAgent import xmlAgent
 
 
 class PersistentDataManager():
+  '''
+  This class handles any information that will remain useful across
+  several boot-ups. When any new categories are created or planned 
+  transactions entered, they are written into an xml file. This job is
+  passed down to the xmlAgent (see for more description). The 
+  PersistentDataManager keeps useful information at the hands of the
+  user by storing it and retrieving it from an xml file.
+  '''
 
   def __init__(self):
     self.xmlAgent = xmlAgent()
